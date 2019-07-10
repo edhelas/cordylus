2bfae9e386672a47bf51acc089502520e2d2c030
Timothée Jaussoin <edhelas@movim.eu>
2019-07-10 18:55:26 +0200

    More CSS fixes

--- resources/views/partials/picture.blade.php
+++ resources/views/partials/picture.blade.php
@@ -7,11 +7,11 @@
             @if(!empty($caption))
                 {{asset($picture->path('blurred', 'webp'))}}
             @else
-                {{asset($picture->path('xxl', 'webp'))}} 1920w,
-                {{asset($picture->path('xl', 'webp'))}} 1536w,
-                {{asset($picture->path('l', 'webp'))}} 1024w,
-                {{asset($picture->path('ml', 'webp'))}} 768w,
-                {{asset($picture->path('sl', 'webp'))}} 512w
+                {{asset($picture->path('xxl', 'webp'))}} 1536w,
+                {{asset($picture->path('xl', 'webp'))}} 1024w,
+                {{asset($picture->path('l', 'webp'))}} 768w,
+                {{asset($picture->path('ml', 'webp'))}} 512w,
+                {{asset($picture->path('sl', 'webp'))}} 256w
             @endif
         "
         type="image/webp">
@@ -20,11 +20,11 @@
             @if(!empty($caption))
                 {{asset($picture->path('blurred'))}}
             @else
-            {{asset($picture->path('xxl'))}} 1920w,
-            {{asset($picture->path('xl'))}} 1536w,
-            {{asset($picture->path('l'))}} 1024w,
-            {{asset($picture->path('ml'))}} 768w,
-            {{asset($picture->path('sl'))}} 512w
+            {{asset($picture->path('xxl'))}} 1536w,
+            {{asset($picture->path('xl'))}} 1024w,
+            {{asset($picture->path('l'))}} 768w,
+            {{asset($picture->path('ml'))}} 512w,
+            {{asset($picture->path('sl'))}} 256w
             @endif
         "
         src="{{asset($picture->path(!empty($caption) ? 'blurred' : 'xl'))}}"
